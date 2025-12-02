41b4f4752b6e5c971cb76953f9c5eadc801f6ea7
Anton Johansson <anjo@rev.ng>
2025-12-02 17:00:10 +0100

    remove Smrnmi from testing
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- build-and-run-qemu-tests.sh
+++ build-and-run-qemu-tests.sh
@@ -25,25 +25,19 @@ pushd build/qemu
 
 make build-xqci-asm-tests
 make build-xqccmp-asm-tests
-make build-smrnmi-asm-tests
 
 make build-xqci-c-tests CC=$clang
 make build-xqccmp-c-tests CC=$clang
-make build-smrnmi-c-tests CC=$clang
 
 make run-xqci-asm-tests > out-xqci-asm-tests
 make run-xqccmp-asm-tests > out-xqccmp-asm-tests
-make run-smrnmi-asm-tests > out-smrnmi-asm-tests
 
 make run-xqci-c-tests > out-xqci-c-tests
 make run-xqccmp-c-tests > out-xqccmp-c-tests
-make run-smrnmi-c-tests > out-smrnmi-c-tests
 
 print_test_result "xqci asm" out-xqci-asm-tests
 print_test_result "xqci c" out-xqci-c-tests
 print_test_result "xqccmp asm" out-xqccmp-asm-tests
 print_test_result "xqccmp c" out-xqccmp-c-tests
-print_test_result "smrnmi asm" out-smrnmi-asm-tests
-print_test_result "smrnmi c" out-smrnmi-c-tests
 
 popd
